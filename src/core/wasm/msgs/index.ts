14459374204807e0a3177a3601880c636442e228
Paul Kim <paul@terra.money>
2020-07-08 22:24:38 +0900

    update wasm codes

--- src/core/wasm/msgs/index.ts
+++ src/core/wasm/msgs/index.ts
@@ -1,19 +1,27 @@
 import { MsgStoreCode } from './MsgStoreCode';
 import { MsgInstantiateContract } from './MsgInstantiateContract';
 import { MsgExecuteContract } from './MsgExecuteContract';
+import { MsgMigrateContract } from './MsgMigrateContract';
+import { MsgUpdateContractOwner } from './MsgUpdateContractOwner';
 
 export * from './MsgStoreCode';
 export * from './MsgInstantiateContract';
 export * from './MsgExecuteContract';
+export * from './MsgMigrateContract';
+export * from './MsgUpdateContractOwner';
 
 export type WasmMsg =
   | MsgStoreCode
   | MsgInstantiateContract
-  | MsgExecuteContract;
+  | MsgExecuteContract
+  | MsgMigrateContract
+  | MsgUpdateContractOwner;
 
 export namespace WasmMsg {
   export type Data =
     | MsgStoreCode.Data
     | MsgInstantiateContract.Data
-    | MsgExecuteContract.Data;
+    | MsgExecuteContract.Data
+    | MsgMigrateContract.Data
+    | MsgUpdateContractOwner.Data;
 }
