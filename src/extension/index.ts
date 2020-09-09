de68d9a2ad23ac5ecfd2fb792df29b4fef15cee0
Paul Kim <paul@terra.money>
2020-09-09 18:41:31 +0900

    Feature: Extension module (#22)
    * version 0.4.14
    * feat(extension): initial commit
    * fix(extension): rename sendMessage to send
    * feat(extension): add connect and post method with strict typing
    * fix(extension): use Date.now for id
    * fix(extension/post): add optional lcdClientConfig parameter

---
+++ src/extension/index.ts
@@ -0,0 +1,72 @@
+import { Msg } from '../core/Msg';
+import { LCDClientConfig } from '../client';
+
+const LocalMessageDuplexStream = require('post-message-stream');
+
+interface ResponseData {
+  name: string;
+  payload: object;
+}
+
+type SendDataType = 'connect' | 'post';
+
+interface SendData {
+  id: number | string;
+  type: SendDataType;
+  [key: string]: any;
+}
+
+// Singleton class for communicating between page and extension
+export class Extension {
+  static instance: Extension;
+  private inpageStream: any;
+
+  constructor() {
+    if (Extension.instance) {
+      return Extension.instance;
+    }
+
+    Extension.instance = this;
+
+    this.inpageStream = new LocalMessageDuplexStream({
+      name: 'station:inpage',
+      target: 'station:content',
+    });
+  }
+
+  // low level function for sending message to extension.
+  // Do not use this function unless you know what you are doing.
+  send(data: SendData): void {
+    this.inpageStream.write(data);
+  }
+
+  on(name: string, callback: (payload: any) => void): void {
+    this.inpageStream.on('data', (data: ResponseData) => {
+      data.name === name && callback(data.payload);
+    });
+  }
+
+  connect(): number {
+    const id = Date.now();
+
+    this.send({
+      id,
+      type: 'connect',
+    });
+
+    return id;
+  }
+
+  post(msgs: Msg[], lcdClientConfig?: LCDClientConfig): number {
+    const id = Date.now();
+
+    this.send({
+      id,
+      type: 'post',
+      msgs: msgs.map(msg => msg.toJSON()),
+      lcdClientConfig,
+    });
+
+    return id;
+  }
+}
