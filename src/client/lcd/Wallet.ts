ee8934e5a1222df2c56f59bd3682c5aadde4790f
Paul Kim <paul@terra.money>
2020-09-09 18:32:29 +0900

    feat(client/lcd): Add accountNumberAndSequence method

--- src/client/lcd/Wallet.ts
+++ src/client/lcd/Wallet.ts
@@ -7,27 +7,46 @@ import { Account, StdTx, StdSignMsg } from '../../core';
 export class Wallet {
   constructor(public lcd: LCDClient, public key: Key) {}
 
-  public async accountNumber(): Promise<number> {
+  public accountNumberAndSequence(): Promise<{
+    account_number: number;
+    sequence: number;
+  }> {
+    return this.lcd.auth.accountInfo(this.key.accAddress).then(d => {
+      if (d instanceof Account) {
+        return {
+          account_number: d.account_number,
+          sequence: d.sequence,
+        };
+      }
+
+      return {
+        account_number: d.BaseAccount.account_number,
+        sequence: d.BaseAccount.sequence,
+      };
+    });
+  }
+
+  public accountNumber(): Promise<number> {
     return this.lcd.auth.accountInfo(this.key.accAddress).then(d => {
       if (d instanceof Account) {
         return d.account_number;
-      } else {
-        return d.BaseAccount.account_number;
       }
+
+      return d.BaseAccount.account_number;
     });
   }
 
-  public async sequence(): Promise<number> {
+  public sequence(): Promise<number> {
     return this.lcd.auth.accountInfo(this.key.accAddress).then(d => {
       if (d instanceof Account) {
         return d.sequence;
-      } else {
-        return d.BaseAccount.sequence;
       }
+
+      return d.BaseAccount.sequence;
     });
   }
 
-  public async createTx(options: CreateTxOptions): Promise<StdSignMsg> {
+  public createTx(options: CreateTxOptions): Promise<StdSignMsg> {
     return this.lcd.tx.create(this.key.accAddress, options);
   }
 
