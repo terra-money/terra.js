acc15b23d2dec4ccac5ccf87203066d3daa87cdc
Paul Kim <paul@terra.money>
2020-11-12 17:24:02 +0900

    fix(key): skip import for browser fix(extension): change different window variable for isAvailable

--- src/key/index.ts
+++ src/key/index.ts
@@ -1,4 +1,13 @@
 export * from './Key';
 export * from './MnemonicKey';
 export * from './RawKey';
-export * from './CLIKey';
+
+let CLIKey: any;
+
+declare const __WEBPACK_BROWSER__: boolean | undefined;
+
+if (typeof __WEBPACK_BROWSER__ !== 'boolean') {
+  ({ CLIKey } = require('./CLIKey'));
+}
+
+export { CLIKey };
