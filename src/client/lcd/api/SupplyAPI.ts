edf68d0e73108ad946e63d72a3d46e2fe948bfed
brycepratt <bbsrpratt@gmail.com>
2021-05-27 17:04:25 +0900

    Migrate supply to new endpoint on Bombay

--- src/client/lcd/api/SupplyAPI.ts
+++ src/client/lcd/api/SupplyAPI.ts
@@ -1,13 +1,23 @@
 import { Coins } from '../../../core';
 import { BaseAPI } from './BaseAPI';
 
+export interface SupplyResponse {
+  supply: Coins;
+}
+
+export namespace SupplyResponse {
+  export interface Data {
+    supply: Coins.Data;
+  }
+}
+
 export class SupplyAPI extends BaseAPI {
   /**
    * Get the total supply of tokens in circulation for all denominations.
    */
   public async total(): Promise<Coins> {
     return this.c
-      .get<Coins.Data>(`/supply/total`)
-      .then(d => Coins.fromData(d.result));
+      .get<SupplyResponse.Data>(`/bank/total`)
+      .then(d => Coins.fromData(d.result.supply));
   }
 }
