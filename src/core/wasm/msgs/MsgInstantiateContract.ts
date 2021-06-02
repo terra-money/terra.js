78b49884da2e670cbbe6a82b254d5f38d81ff9ff
brycepratt <bbsrpratt@gmail.com>
2021-06-02 17:36:20 +0900

    Apply bombay changes to MsgInstantiateContract

--- src/core/wasm/msgs/MsgInstantiateContract.ts
+++ src/core/wasm/msgs/MsgInstantiateContract.ts
@@ -3,26 +3,22 @@ import { AccAddress } from '../../strings';
 import { Coins } from '../../Coins';
 import { b64ToDict, dictToB64 } from '../../../util/contract';
 
-export class MsgInstantiateContract extends JSONSerializable<
-  MsgInstantiateContract.Data
-> {
+export class MsgInstantiateContract extends JSONSerializable<MsgInstantiateContract.Data> {
   public init_coins: Coins;
 
   /**
-   * @param creator contract creator
-   * @param admin is optional contract admin address who can migrate contract
-   * @param code_id reference to the code on the blockchain
-   * @param init_msg message to configure the initial state of the contract
-   * @param init_coins initial amount of coins to be sent to the contract's address
-   * @param migratable defines to be migratable or not
+   * @param sender is a sender address
+   * @param admin is an optional contract admin address who can migrate the contract
+   * @param code_id is the reference to the stored WASM code
+   * @param init_msg json encoded message to be passed to the contract on instantiation
+   * @param init_coins are transferred to the contract on execution
    */
   constructor(
-    public creator: AccAddress,
+    public sender: AccAddress,
     public admin: AccAddress,
     public code_id: number,
     public init_msg: object,
-    init_coins: Coins.Input = {},
-    public migratable: boolean = false
+    init_coins: Coins.Input = {}
   ) {
     super();
     this.init_coins = new Coins(init_coins);
@@ -32,29 +28,27 @@ export class MsgInstantiateContract extends JSONSerializable<
     data: MsgInstantiateContract.Data
   ): MsgInstantiateContract {
     const {
-      value: { creator, admin, code_id, init_msg, init_coins, migratable },
+      value: { sender, admin, code_id, init_msg, init_coins },
     } = data;
     return new MsgInstantiateContract(
-      creator,
+      sender,
       admin,
       Number.parseInt(code_id),
       b64ToDict(init_msg),
-      Coins.fromData(init_coins),
-      migratable
+      Coins.fromData(init_coins)
     );
   }
 
   public toData(): MsgInstantiateContract.Data {
-    const { creator, admin, code_id, init_msg, init_coins, migratable } = this;
+    const { sender, admin, code_id, init_msg, init_coins } = this;
     return {
       type: 'wasm/MsgInstantiateContract',
       value: {
-        creator,
+        sender,
         admin,
         code_id: code_id.toFixed(),
         init_msg: dictToB64(init_msg),
         init_coins: init_coins.toData(),
-        migratable,
       },
     };
   }
@@ -64,12 +58,11 @@ export namespace MsgInstantiateContract {
   export interface Data {
     type: 'wasm/MsgInstantiateContract';
     value: {
-      creator: AccAddress;
+      sender: AccAddress;
       admin: AccAddress;
       code_id: string;
       init_msg: string;
       init_coins: Coins.Data;
-      migratable: boolean;
     };
   }
 }
