d57f44dc4be5a138991b5024213701638bcc356d
Paul Kim <paul@terra.money>
2021-06-20 14:37:29 +0900

    fix: tsc error

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -4,7 +4,7 @@ export * from './Account';
 export * from './LazyGradedVestingAccount';
 
 export function isVesting(
-  account: Account | LazyGradedVestingAccount
-): account is LazyGradedVestingAccount {
-  return account instanceof LazyGradedVestingAccount;
+  acc: Account | LazyGradedVestingAccount
+): acc is LazyGradedVestingAccount {
+  return acc instanceof LazyGradedVestingAccount;
 }
