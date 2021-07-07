13d8194d5c819bbc31c8dbe3deff773a5ab52036
Paul Kim <paul@terra.money>
2021-07-07 19:17:02 +0900

    test(api/StakingAPI): add more tests fix(LCDClient): default gas prices to 0.015uluna

--- src/client/lcd/api/StakingAPI.spec.ts
+++ src/client/lcd/api/StakingAPI.spec.ts
@@ -1,7 +1,9 @@
 import { APIRequester } from '../APIRequester';
 import { StakingAPI } from './StakingAPI';
+import { Dec, Int } from '../../../core/numeric';
+import { Coin } from '../../../core/Coin';
 
-const c = new APIRequester('https://lcd.terra.dev/');
+const c = new APIRequester('https://tequila-lcd.terra.dev/');
 const staking = new StakingAPI(c);
 
 describe('StakingAPI', () => {
@@ -12,6 +14,104 @@ describe('StakingAPI', () => {
       max_entries: expect.any(Number),
       historical_entries: expect.any(Number),
       bond_denom: expect.any(String),
+    });
+  });
+
+  it('delegations (delegator)', async () => {
+    await expect(
+      staking.delegations('terra1940nsxkz62snd3azk3a9j79m4qd3qvwnrf2xvj')
+    ).resolves.toContainEqual({
+      delegator_address: expect.any(String),
+      validator_address: expect.any(String),
+      shares: expect.any(Dec),
+      balance: expect.any(Coin),
+    });
+  });
+
+  it('delegations (validator)', async () => {
+    await expect(
+      staking.delegations(
+        undefined,
+        'terravaloper1vk20anceu6h9s00d27pjlvslz3avetkvnwmr35'
+      )
+    ).resolves.toContainEqual({
+      delegator_address: expect.any(String),
+      validator_address: expect.any(String),
+      shares: expect.any(Dec),
+      balance: expect.any(Coin),
+    });
+  });
+
+  it('redelegations', async () => {
+    await expect(staking.redelegations()).resolves.toContainEqual({
+      delegator_address: expect.any(String),
+      validator_src_address: expect.any(String),
+      validator_dst_address: expect.any(String),
+      entries: expect.arrayContaining([
+        {
+          initial_balance: expect.any(Int),
+          balance: expect.any(Int),
+          shares_dst: expect.any(Dec),
+          creation_height: expect.any(Number),
+          completion_time: expect.any(Date),
+        },
+      ]),
+    });
+  });
+
+  it('unbondingDelegations', async () => {
+    await expect(
+      staking.unbondingDelegations(
+        undefined,
+        'terravaloper1vk20anceu6h9s00d27pjlvslz3avetkvnwmr35'
+      )
+    ).resolves.toContainEqual({
+      delegator_address: expect.any(String),
+      validator_address: expect.any(String),
+      entries: expect.arrayContaining([
+        {
+          initial_balance: expect.any(Int),
+          balance: expect.any(Int),
+          creation_height: expect.any(Number),
+          completion_time: expect.any(Date),
+        },
+      ]),
+    });
+  });
+
+  it('validators', async () => {
+    await expect(staking.validators()).resolves.toContainEqual({
+      operator_address: expect.any(String),
+      consensus_pubkey: expect.any(String),
+      jailed: expect.any(Boolean),
+      status: expect.any(Number),
+      tokens: expect.any(Int),
+      delegator_shares: expect.any(Dec),
+      description: {
+        moniker: expect.any(String),
+        identity: expect.any(String),
+        website: expect.any(String),
+        details: expect.any(String),
+        security_contact: expect.any(String),
+      },
+      unbonding_height: expect.any(Number),
+      unbonding_time: expect.any(Date),
+      commission: {
+        commission_rates: {
+          rate: expect.any(Dec),
+          max_rate: expect.any(Dec),
+          max_change_rate: expect.any(Dec),
+        },
+        update_time: expect.any(Date),
+      },
+      min_self_delegation: expect.any(Int),
+    });
+  });
+
+  it('pool', async () => {
+    await expect(staking.pool()).resolves.toMatchObject({
+      bonded_tokens: expect.any(Coin),
+      not_bonded_tokens: expect.any(Coin),
     });
   });
 });
