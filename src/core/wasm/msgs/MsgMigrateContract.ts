b9de1217145637fd0d81eb25f6b5aae8b8aadff9
Paul Kim <paul@terra.money>
2021-08-18 19:01:38 +0900

    fix(wasm): make MsgInstantiateContract and MsgMigrateContract compatible with bombay build: bump version to 1.8.10

--- src/core/wasm/msgs/MsgMigrateContract.ts
+++ src/core/wasm/msgs/MsgMigrateContract.ts
@@ -9,28 +9,44 @@ export class MsgMigrateContract extends JSONSerializable<MsgMigrateContract.Data
    * @param migrate_msg JSON message to configure the migrate state of the contract
    */
   constructor(
-    public owner: AccAddress,
+    public owner: AccAddress | undefined,
     public contract: AccAddress,
     public new_code_id: number,
-    public migrate_msg: object // json object
+    public migrate_msg: object, // json object
+    public admin: AccAddress | undefined
   ) {
     super();
   }
 
   public static fromData(data: MsgMigrateContract.Data): MsgMigrateContract {
     const {
-      value: { owner, contract, new_code_id, migrate_msg },
+      value: { owner, contract, new_code_id, migrate_msg, admin },
     } = data;
     return new MsgMigrateContract(
       owner,
       contract,
       Number.parseInt(new_code_id),
-      b64ToDict(migrate_msg)
+      b64ToDict(migrate_msg),
+      admin
     );
   }
 
   public toData(): MsgMigrateContract.Data {
-    const { owner, contract, new_code_id, migrate_msg } = this;
+    const { owner, contract, new_code_id, migrate_msg, admin } = this;
+
+    if (typeof admin !== 'undefined') {
+      // for bombay
+      return {
+        type: 'wasm/MsgMigrateContract',
+        value: {
+          admin,
+          contract,
+          new_code_id: new_code_id.toFixed(),
+          migrate_msg,
+        },
+      };
+    }
+
     return {
       type: 'wasm/MsgMigrateContract',
       value: {
@@ -47,10 +63,12 @@ export namespace MsgMigrateContract {
   export interface Data {
     type: 'wasm/MsgMigrateContract';
     value: {
-      owner: AccAddress;
+      // For bombay compatibility owner and admin are optional. migrate msg can be string or object
+      owner?: AccAddress;
       contract: AccAddress;
       new_code_id: string;
-      migrate_msg: string;
+      migrate_msg: string | object;
+      admin?: AccAddress;
     };
   }
 }
