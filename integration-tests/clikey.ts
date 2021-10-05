72550c8d273573bbbb146b7c6cf06fe0e84941df
Yun Yeo <sw.yunsuk@gmail.com>
2021-10-05 12:07:44 +0900

    rollback test

--- integration-tests/clikey.ts
+++ integration-tests/clikey.ts
@@ -5,23 +5,20 @@ import { CLIKey } from '../src/key/CLIKey';
 const terra = new LocalTerra();
 const { test1 } = terra.wallets;
 const cliKey = new CLIKey({ keyName: 'operator' });
-console.log(cliKey.valAddress);
-console.log(cliKey.accAddress);
-console.log(cliKey.publicKey);
 
-// const cliWallet = terra.wallet(cliKey);
+const cliWallet = terra.wallet(cliKey);
 
-// const send = new MsgSend(cliWallet.key.accAddress, test1.key.accAddress, {
-//   uluna: 100000,
-// });
+const send = new MsgSend(cliWallet.key.accAddress, test1.key.accAddress, {
+  uluna: 100000,
+});
 
-// async function main() {
-//   const tx = await cliWallet.createAndSignTx({
-//     msgs: [send],
-//     fee: new Fee(100000, { uluna: 100000 }, '', ''),
-//   });
+async function main() {
+  const tx = await cliWallet.createAndSignTx({
+    msgs: [send],
+    fee: new Fee(100000, { uluna: 100000 }, '', ''),
+  });
 
-//   console.log(await terra.tx.broadcast(tx));
-// }
+  console.log(await terra.tx.broadcast(tx));
+}
 
-// main().catch(console.error);
+main().catch(console.error);
