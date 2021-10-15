88c2163c64a8bd1eb52469939f8f947e1cfc9ce5
Geoff Lee <geoff@terra.money>
2021-10-15 15:25:35 +0900

    mod spec for MsgTransfer due to timeout_height

--- src/core/ibc-transfer/msgs/MsgTransfer.spec.ts
+++ src/core/ibc-transfer/msgs/MsgTransfer.spec.ts
@@ -1,6 +1,5 @@
 import { MsgTransfer } from './MsgTransfer';
 import { Coin } from '../../Coin';
-import Long from 'long';
 
 describe('MsgTransfer', () => {
   it('deserializes correctly', () => {
@@ -25,8 +24,8 @@ describe('MsgTransfer', () => {
       sender: 'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
       receiver: 'recvr17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
       timeout_height: {
-        revision_number: new Long(0),
-        revision_height: new Long(0),
+        revision_number: 0,
+        revision_height: 0,
       },
       timeout_timestamp: 1631618921,
     });
