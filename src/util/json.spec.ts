393c2a18d736d524754e0c2f832cde9374bc6c99
David <82286764+decryptdavid@users.noreply.github.com>
2021-10-27 06:51:41 +0100

    Removed Object mutation from `removeNull` utility and increased code coverage (#156)
    * Removed Object mutation from removeNull utility and increased code coverage

--- src/util/json.spec.ts
+++ src/util/json.spec.ts
@@ -1,25 +1,26 @@
 import { removeNull } from './json';
 
 describe('removeNull', () => {
-  it('remove null ', () => {
-    expect(
-      removeNull({
-        a: 'abc',
-        b: {
-          a: null,
-          b: 123,
-        },
-        c: null,
-        d: [123],
-        e: {
-          a: {
-            a: null,
-            b: 'abc',
-          },
-          b: 123,
-        },
-      })
-    ).toEqual({
+  const object = {
+    a: 'abc',
+    b: {
+      a: null,
+      b: 123,
+    },
+    c: null,
+    d: [123],
+    e: {
+      a: {
+        a: null,
+        b: 'abc',
+      },
+      b: 123,
+    },
+  };
+
+  it('removes null values from object', () => {
+    const newObject = removeNull(object);
+    expect(newObject).toEqual({
       a: 'abc',
       b: {
         b: 123,
@@ -32,5 +33,20 @@ describe('removeNull', () => {
         b: 123,
       },
     });
+  });
+
+  it('does not mutate object', () => {
+    removeNull(object);
+    expect(object).toEqual(object);
+  });
+
+  it('returns object or primative if not Object', () => {
+    const string = 'string';
+    const number = 1;
+    const boolean = false;
+
+    expect(removeNull(string)).toEqual(string);
+    expect(removeNull(number)).toEqual(number);
+    expect(removeNull(boolean)).toEqual(boolean);
   });
 });
