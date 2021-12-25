d80a12334e516f192de06774aeb73cbaba821313
Geoff Lee <geoff@terra.money>
2021-12-25 18:40:20 +0900

    temporary commit: sign doesn't work

--- integration-tests/ledgerkey.ts
+++ integration-tests/ledgerkey.ts
@@ -1,16 +1,14 @@
-import { LCDClient, MsgSend, LedgerKey } from '../src';
+import { LCDClient, MsgSend, LedgerKey, LedgerTransportType } from '../src';
 import { SignMode } from '@terra-money/terra.proto/cosmos/tx/signing/v1beta1/signing';
-import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
 
 async function main() {
   // ledgerkey
-  const transport = await TransportNodeHid.create()
-  const lk = new LedgerKey(transport);
+  const lk = new LedgerKey(LedgerTransportType.NODE);
+  await lk.initialize();
 
   const bombay = new LCDClient({
     chainID: 'bombay-12',
     URL: 'https://bombay-lcd.terra.dev',
-    gasPrices: { uusd: 0.38 },
   });
 
   // a wallet can be created out of any key
@@ -19,23 +17,20 @@ async function main() {
 
   // create a simple message that moves coin balances
   const send = new MsgSend(
+    'terra1mzv70x2avy4k95dujh9j3xh43nusxh8mh02cs5',
     'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
-    'terra17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
-    { uusd: 1312029 }
+    { uluna: 120400 }
   );
 
-  wallet
+  const tx = await wallet
     .createAndSignTx({
-      signMode: SignMode.SIGN_MODE_LEGACY_AMINO_JSON,
       msgs: [send],
-      memo: 'test from terra.js!',
-    })
-    .then(tx => {
-      return bombay.tx.broadcast(tx);
-    })
-    .then(result => {
-      console.log(`TX hash: ${result.txhash}  ${result.raw_log}`);
+      memo: 'ledgerkey test',
+      signMode: SignMode.SIGN_MODE_LEGACY_AMINO_JSON
     });
+  console.log(`signed tx: ${JSON.stringify(tx)}`);
+  const result = await bombay.tx.broadcast(tx);
+  console.log(`TX hash: ${result.txhash}  ${result.raw_log}`);
 }
 
 main().catch(console.error);
--- src/key/LedgerKey.ts
+++ src/key/LedgerKey.ts
@@ -5,14 +5,22 @@ import { SignatureV2 } from '../core/SignatureV2';
 import { SimplePublicKey } from '../core/PublicKey';
 
 import Transport from '@ledgerhq/hw-transport';
-import TerraApp, { PublicKeyResponse } from '@terra-money/ledger-terra-js';
-import { ERROR_CODE } from '@terra-money/ledger-terra-js';
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
+import TransportNodeHidNoEvents from '@ledgerhq/hw-transport-node-hid-noevents';
+import TransportNodeHidSingleton from '@ledgerhq/hw-transport-node-hid-singleton';
+import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
+import TerraApp from '@terra-money/ledger-terra-js';
+//import { ERROR_CODE } from '@terra-money/ledger-terra-js';
+const NoError = 0x9000;
 import { LUNA_COIN_TYPE } from './MnemonicKey';
 import { SignMode } from '../client/lcd/Wallet';
 
-interface LedgerKeyParams {
-  openTimeout?: number;
-  listenTimeout?: number;
+export enum LedgerTransportType {
+  WEB_USB = 1,
+  NODE,
+  //NODE_HID_SINGLETON,
+  //NODE_HID,
+  //NODE_HID_NO_EVENTS,
 }
 
 /**
@@ -23,134 +31,95 @@ interface LedgerKeyParams {
  * for `sign()`.
  */
 export class LedgerKey extends Key {
-  private _transport: Transport;
-  private app: TerraApp;
+  //private app: TerraApp;
   private _accAddress?: AccAddress;
+  //private app: TerraApp;
   private path: number[] = [44, LUNA_COIN_TYPE, 0, 0, 0];
+  static transport?: LedgerTransportType;
 
   /**
    *
    * @param transport transporter for LedgerKey
    */
-  constructor(private transport: Transport) {
+  constructor(public transport: LedgerTransportType) {
     super();
-    this._transport = transport;
-    this.app = new TerraApp(transport);
-    this.initialize()
-      .then(() => {
-        console.log('initialized');
-      })
-      .catch(reason => {
-        console.log(reason);
-      });
-    /*
-    this.app.initialize().then(res => {
-      if (res == null || res.return_code != ERROR_CODE.NoError) {
-        let reason = null;
-        if (res != null) reason = res.error_message
-        throw new Error(
-          `Can't initialize LedgerKey ` + reason
-        );
-      }
-    }).catch(reason => {
-      console.log(reason);
-    });
-    */
+    //this.app = new TerraApp(this.transport)
+    //this.app.initialize().then((res) => { console.log(res) });
+  }
+
+  public async initialize() {
+    await this.loadAccountDetails();
+  }
+
+  private async getTransport(
+    transportType?: LedgerTransportType
+  ): Promise<Transport> {
+    if (transportType == undefined) {
+      return this.getTransport(this.transport);
+    }
+    switch (transportType) {
+      case LedgerTransportType.WEB_USB:
+        return await TransportWebUSB.create();
+      case LedgerTransportType.NODE:
+        return await TransportNodeHidSingleton.create();
+      /*
+      case LedgerTransportType.NODE_HID:
+        return await TransportNodeHid.create();
+      case LedgerTransportType.NODE_HID_NO_EVENTS:
+        return await TransportNodeHidNoEvents.create();
+        */
+      //case LedgerTransportType.NODE_HID_SINGLETON:
+      //  return await TransportNodeHidSingleton.create();
+      default:
+        throw new Error('invalid transport type');
+    }
   }
 
   /**
    * initialize ledger
    */
-  private async initialize() {
-    const res = await this.app.initialize();
-    console.log('initialized!');
-    console.log(res);
-    /*
-    if (res == null || res.return_code != ERROR_CODE.NoError) {
-      let reason = null;
-      if (res != null) reason = res.error_message
-      throw new Error(
-        `Can't initialize LedgerKey ` + reason
-      );
-    }
-    */
-  }
-
-  private async loadAccountDetails() {
-    /*
-    const res = await this.app.initialize()
-    if (res == null || res.return_code != ERROR_CODE.NoError) {
-      let reason = null;
-      if (res != null) reason = res.error_message
-      throw new Error(
-        `Can't initialize LedgerKey ` + reason
-      );
-    }
-    */
-    const res = await this.app.getAddressAndPubKey(this.path, 'terra');
-    if (res == null || res.return_code != 0) {
+  private async getTerraApp(): Promise<TerraApp> {
+    const app = new TerraApp(await this.getTransport());
+    const res = await app.initialize();
+    if (res != null && res.return_code != /*ERROR_CODE.*/ NoError) {
       let reason = null;
       if (res != null) reason = res.error_message;
-      throw new Error(`Can't get address and public key. ` + reason);
+      throw new Error(`Can't initialize LedgerKey ` + reason);
     }
+    return app;
+  }
+
+  public async loadAccountDetails() {
+    const app = await this.getTerraApp();
+    const res = await app.getAddressAndPubKey(this.path, 'terra');
+    if (res.return_code != NoError) {
+      throw new Error(
+        `Can't get address and public key. ${JSON.stringify(res)}`
+      );
+    }
+
     this._accAddress = res.bech32_address;
     this.publicKey = new SimplePublicKey(
       Buffer.from(res.compressed_pk.data).toString('base64')
     );
-  }
-
-  /**
-   * Terra account address. `terra-` prefixed.
-   */
-  public get accAddress(): AccAddress {
-    if (!this._accAddress) {
-      this.loadAccountDetails();
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return this._accAddress!;
-  }
-
-  /**
-   * Terra validator address. `terravaloper-` prefixed.
-   */
-  public get valAddress(): ValAddress {
-    if (!this._accAddress) {
-      this.loadAccountDetails();
-      this._accAddress = this.accAddress;
-    }
-    return ValAddress.fromAccAddress(this._accAddress);
+    //console.log(`accAddress: ${this._accAddress}`)
+    //console.log(`publicKey: ${JSON.stringify(this.publicKey)}`)
   }
 
   public async sign(message: Buffer): Promise<Buffer> {
     if (!this.publicKey) {
       this.loadAccountDetails();
     }
-    const res = await this.app.sign(this.path, message);
-    if (res.return_code != ERROR_CODE.NoError) {
-      throw new Error(`Can't sign a message. return_cdode:${res.return_code}`);
+    const app = await this.getTerraApp();
+    const res = await app.sign(this.path, message);
+
+    if (res.return_code != /*ERROR_CODE.*/ NoError) {
+      throw new Error(`Can't sign a message. ${JSON.stringify(res)}`);
     }
     return Buffer.from(res.signature.data);
   }
-
-  public async createSignature(tx: SignDoc): Promise<SignatureV2> {
-    const txStr = JSON.stringify(tx.toUnSignedTx().toData());
-    const signature = await this.sign(Buffer.from(txStr));
-
-    return new SignatureV2(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      this.publicKey!,
-      new SignatureV2.Descriptor(
-        new SignatureV2.Descriptor.Single(
-          SignMode.SIGN_MODE_DIRECT,
-          signature.toString('base64')
-        )
-      ),
-      tx.sequence
-    );
-  }
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async createSignatureAmino(_tx: SignDoc): Promise<SignatureV2> {
-    throw new Error('amino sign mode is not supported');
+  public async createSignature(_tx: SignDoc): Promise<SignatureV2> {
+    throw new Error('direct sign mode is not supported');
   }
 }
