e6b217a96ab96f3ee7b0aa9ed105748de285510c
Geoff Lee <geoff@terra.money>
2022-01-20 15:15:38 +0900

    add spec for MsgTransfer

--- src/core/ibc-transfer/msgs/MsgTransfer.spec.ts
+++ src/core/ibc-transfer/msgs/MsgTransfer.spec.ts
@@ -1,5 +1,6 @@
 import { MsgTransfer } from './MsgTransfer';
 import { Coin } from '../../Coin';
+import { Height } from '../../ibc/msgs/client/Height';
 
 describe('MsgTransfer', () => {
   it('deserializes correctly', () => {
@@ -28,6 +29,66 @@ describe('MsgTransfer', () => {
         revision_height: 0,
       },
       timeout_timestamp: 1631618921,
+    });
+  });
+
+  it('deserializes amino without timeout_height', () => {
+    const send = MsgTransfer.fromData({
+      '@type': '/ibc.applications.transfer.v1.MsgTransfer',
+      source_port: 'sourceport-01',
+      source_channel: 'sourcehannel-01',
+      token: { denom: 'uluna', amount: '1024' },
+      sender: 'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
+      receiver: 'recvr17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
+      timeout_height: {
+        revision_number: '0',
+        revision_height: '0',
+      },
+      timeout_timestamp: '1631618921',
+    });
+    const aminoSend = send.toAmino();
+
+    expect(aminoSend).toMatchObject({
+      type: 'cosmos-sdk/MsgTransfer',
+      value: {
+        source_port: 'sourceport-01',
+        source_channel: 'sourcehannel-01',
+        token: new Coin('uluna', '1024').toAmino(),
+        sender: 'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
+        receiver: 'recvr17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
+        timeout_timestamp: '1631618921',
+      },
+    });
+
+    expect(send.toData()).toMatchObject({});
+  });
+
+  it('deserializes amino without timeout_timestamp', () => {
+    const send = MsgTransfer.fromData({
+      '@type': '/ibc.applications.transfer.v1.MsgTransfer',
+      source_port: 'sourceport-01',
+      source_channel: 'sourcehannel-01',
+      token: { denom: 'uluna', amount: '1024' },
+      sender: 'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
+      receiver: 'recvr17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
+      timeout_height: {
+        revision_number: '5',
+        revision_height: '57240001',
+      },
+      timeout_timestamp: '0',
+    });
+    const aminoSend = send.toAmino();
+
+    expect(aminoSend).toMatchObject({
+      type: 'cosmos-sdk/MsgTransfer',
+      value: {
+        source_port: 'sourceport-01',
+        source_channel: 'sourcehannel-01',
+        token: new Coin('uluna', '1024').toAmino(),
+        sender: 'terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v',
+        receiver: 'recvr17lmam6zguazs5q5u6z5mmx76uj63gldnse2pdp',
+        timeout_height: new Height(5, 57240001).toAmino(),
+      },
     });
 
     expect(send.toData()).toMatchObject({});
